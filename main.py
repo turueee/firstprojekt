a5104d1d2dfa9520332c422f8e595b7d2a679d45
turueee <turushkine@inbox.ru>
2024-08-06 10:46:06 +0300

    Создан .gitignore

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from weather_module import get_current_weather
 import telebot
 
-#API_TOKEN = '7278848607:AAG6PS3r_eg_xBtRRBC3Wp1czzMLRSHSBmw'
-#bot = telebot.TeleBot(API_TOKEN)
+
+# API_TOKEN = '7278848607:AAG6PS3r_eg_xBtRRBC3Wp1czzMLRSHSBmw'
+# bot = telebot.TeleBot(API_TOKEN)
 
 
 # Handle '/start' and '/help'
@@ -18,14 +19,15 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
     try:
-        s,r = map(float,message.text.split())
+        s, r = map(float, message.text.split())
     except:
-        bot.reply_to(message,'Координаты введены неправильно!')
-    latitude, longitude = map(float,message.text.split())
-    try:get_current_weather(latitude,longitude)
+        bot.reply_to(message, 'Координаты введены неправильно!')
+    latitude, longitude = map(float, message.text.split())
+    try:
+        get_current_weather(latitude, longitude)
     except:
-        bot.reply_to(message,'Введены несуществующие координаты!')
-    bot.reply_to(message, get_current_weather(latitude,longitude))
+        bot.reply_to(message, 'Введены несуществующие координаты!')
+    bot.reply_to(message, get_current_weather(latitude, longitude))
 
 
 bot.infinity_polling()
