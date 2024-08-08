7ad4fa35f2df4c45867b1cb412149a121c8a2c7d
turueee <turushkine@inbox.ru>
2024-08-08 11:01:39 +0300

    Подключение value_module

---
+++ value_module.py
@@ -0,0 +1,19 @@
+import requests
+import json
+import datetime
+
+
+def value_moneys(sum_money, Charcode1, Charcode2):
+    response = requests.get(
+        'https://www.cbr-xml-daily.ru/daily_json.js'
+    )
+    values = response.json()
+    if Charcode1=='RUB':
+        curse1=1
+    else:
+        curse1 = float(values['Valute'][Charcode1]['Value'])
+    if  Charcode2=='RUB':
+        curse2=1
+    else:
+        curse2 = float(values['Valute'][Charcode2]['Value'])
+    return float(sum_money)*curse1 / curse2
