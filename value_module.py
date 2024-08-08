ecb279326ed479485bb1a8185c795a05c8f112ac
turueee <turushkine@inbox.ru>
2024-08-08 11:28:53 +0300

    Исправление ошибки в value_module

--- value_module.py
+++ value_module.py
@@ -8,12 +8,12 @@ def value_moneys(sum_money, Charcode1, Charcode2):
         'https://www.cbr-xml-daily.ru/daily_json.js'
     )
     values = response.json()
-    if Charcode1=='RUB':
-        curse1=1
+    if Charcode1 == 'RUB':
+        curse1 = 1
     else:
-        curse1 = float(values['Valute'][Charcode1]['Value'])
-    if  Charcode2=='RUB':
-        curse2=1
+        curse1 = float(values['Valute'][Charcode1]['Value']) / int(values['Valute'][Charcode1]['Nominal'])
+    if Charcode2 == 'RUB':
+        curse2 = 1
     else:
-        curse2 = float(values['Valute'][Charcode2]['Value'])
-    return float(sum_money)*curse1 / curse2
+        curse2 = float(values['Valute'][Charcode2]['Value']) / int(values['Valute'][Charcode2]['Nominal'])
+    return float(sum_money) * curse1 / curse2
